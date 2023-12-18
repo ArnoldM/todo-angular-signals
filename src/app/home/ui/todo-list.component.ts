9e33afd07bed36cdc9924dfd77dff297e4d3a3e3
ArnoldM <arnold.messifet@gmail.com>
2023-12-18 11:39:14 +0100

    Add toggle todo state feature

--- src/app/home/ui/todo-list.component.ts
+++ src/app/home/ui/todo-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Todo } from '../../shared/interfaces/todo';
 import { RouterLink } from '@angular/router';
+import { NgStyle } from '@angular/common';
 
 @Component({
   standalone: true,
@@ -10,13 +11,20 @@ import { RouterLink } from '@angular/router';
       @for (todo of todos; track todo.id) {
       <li>
         <a [routerLink]="['/detail', todo.id]">{{ todo.title }}</a>
+        &nbsp;
+        <button
+          (click)="toggleTodoState.emit(todo)"
+          [ngStyle]="{ 'background-color': todo.completed ? 'green' : 'red' }"
+        >
+          @if (todo.completed) { Completed } @else { Todo }
+        </button>
       </li>
       } @empty {
       <li>Nothing to do!</li>
       }
     </ul>
   `,
-  imports: [RouterLink],
+  imports: [RouterLink, NgStyle],
   styles: [
     `
       ul {
@@ -28,4 +36,5 @@ import { RouterLink } from '@angular/router';
 })
 export class TodoListComponent {
   @Input({ required: true }) todos!: Todo[];
+  @Output() toggleTodoState = new EventEmitter<Todo>();
 }
