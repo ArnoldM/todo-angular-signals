af3e2a7b8a80aa0e651126d5679f530bcb151bf8
ArnoldM <arnold.messifet@gmail.com>
2023-12-18 08:36:39 +0100

    Add todo crate feature

---
+++ src/app/home/ui/todo-form.component.ts
@@ -0,0 +1,33 @@
+import { Component, EventEmitter, Output, inject } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { Todo } from '../../shared/interfaces/todo';
+
+@Component({
+  standalone: true,
+  selector: 'app-todo-form',
+  template: `
+    <form
+      [formGroup]="todoForm"
+      (ngSubmit)="todoSubmitted.emit(todoForm.getRawValue())"
+    >
+      <input type="text" formControlName="title" placeholder="title..." />
+      <input
+        type="text"
+        formControlName="description"
+        placeholder="description..."
+      />
+      <button [disabled]="!todoForm.valid" type="submit">Add Todo</button>
+    </form>
+  `,
+  imports: [ReactiveFormsModule],
+})
+export class TodoFormComponent {
+  private fb = inject(FormBuilder);
+
+  todoForm = this.fb.nonNullable.group({
+    title: ['', Validators.required],
+    description: [''],
+  });
+
+  @Output() todoSubmitted = new EventEmitter<Todo>();
+}
